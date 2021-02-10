5ba80f8fed790cfa66430e1804c9c9f95555b194
TR-76 <tr-76@gmx.at>
2021-02-10 16:24:38 +0100

    Add Bromite 88.0.4324.149 arm_systemWebView.apk
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- BromiteSystemWebView/Android.mk
+++ BromiteSystemWebView/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := BromiteSystemWebView
-LOCAL_SRC_FILES := arm64_SystemWebView.apk
+ifeq ($(TARGET_CPU_ABI), arm64-v8a)
+    LOCAL_SRC_FILES := arm64_SystemWebView.apk
+else
+    LOCAL_SRC_FILES := arm_SystemWebView.apk
+endif
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
