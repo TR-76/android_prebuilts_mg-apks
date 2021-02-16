9fa2cc2885e7b0976e5fbb29f4be73675295188d
TR-76 <tr-76@gmx.at>
2021-02-16 16:29:39 +0100

    Add TARGET_EXCLUDES_PREBUILT
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- config.mk
+++ config.mk
@@ -3,11 +3,27 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Prebuilt Packages
 PRODUCT_PACKAGES += \
-    BromiteSystemWebView \
-    EtarCalendar \
-    K9Mail \
-    QKSMS \
+    BromiteSystemWebView
+
+ifneq ($(TARGET_EXCLUDES_PREBUILT_ETAR_CALENDAR),true)
+PRODUCT_PACKAGES += \
+    EtarCalendar
+endif
+
+ifneq ($(TARGET_EXCLUDES_PREBUILT_K9-MAIL),true)
+PRODUCT_PACKAGES += \
+    K9Mail
+endif
+
+ifneq ($(TARGET_EXCLUDES_PREBUILT_QKSMS),true)
+PRODUCT_PACKAGES += \
+    QKSMS
+endif
+
+ifneq ($(TARGET_EXCLUDES_PREBUILT_SIMPLE_KEYBOARD),true)
+PRODUCT_PACKAGES += \
     SimpleKeyboard
+endif
 
 # MicroG Packages    
 PRODUCT_PACKAGES += \
