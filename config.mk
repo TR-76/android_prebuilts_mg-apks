05b97ee8e01491fca2f1ccfcf59aa785e2c90b4e
TR-76 <tr-76@gmx.at>
2022-09-24 14:56:52 +0200

    Add TARGET_EXCLUDES_PREBUILT
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- config.mk
+++ config.mk
@@ -2,8 +2,10 @@ LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Prebuilt Packages
+ifneq ($(TARGET_EXCLUDES_PREBUILT_BROMITE_WEB_VIEW),true)
 PRODUCT_PACKAGES += \
     BromiteSystemWebView
+endif
 
 # MicroG Packages    
 PRODUCT_PACKAGES += \
