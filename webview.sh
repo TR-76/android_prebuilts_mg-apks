79e64a1eb1f7ff68a0177cd6f888a3a080f0ccd6
TR-76 <tr-76@gmx.net>
2025-02-05 18:07:03 +0100

    Update .sh file
    Signed-off-by: TR-76 <tr-76@gmx.net>

--- webview.sh
+++ webview.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 #
 WORKING_DIR=/media/tr/daten/android/diverses/android_cromitewebview/cromitewebview
-#CROMITE_ARM=$WORKING_DIR/CromiteWebview_arm.apk
-CROMITE_ARM64=$WORKING_DIR/CromiteWebview_arm64.apk
+#
 # Colors
 yellow='\033[0;33m'
 nocol='\033[0m'
 #
-#clear
 echo -e "$yellow"
 echo -e "Download Cromite WebView (arm64) from https://github.com/uazo/cromite ...$nocol"
 echo
-
-LOCATION=$(curl -s https://api.github.com/repos/uazo/cromite/releases/latest \
-| grep "arm64_SystemWebView64.apk" \
-| awk '{ print $2 }' \
-| sed 's/,$//'       \
-| sed 's/"//g' )     \
-; wget $LOCATION -O $CROMITE_ARM64
+#
+DOWNLOAD_URL=$(curl -s https://api.github.com/repos/uazo/cromite/releases/latest \
+        | grep browser_download_url \
+        | grep arm64_SystemWebView.apk \
+        | cut -d '"' -f 4)
+curl -s -L --create-dirs -o $WORKING_DIR/CromiteWebview_arm64.apk "$DOWNLOAD_URL"
 #
